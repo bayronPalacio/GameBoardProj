7ef780fcba26770744d08b1ec15fc0c979363257
haonan <haonancaochn@gmail.com>
2020-09-24 22:58:48 -0700

    Test

---
+++ app/src/main/java/com/example/gameboardproj/setMCActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gameboardproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class setMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_mc)
+    }
+}
