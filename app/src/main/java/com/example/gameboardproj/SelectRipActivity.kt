b360dc511271c934540dac8e235d2e1235ff5bf0
haonan <haonancaochn@gmail.com>
2020-09-28 18:06:25 -0700

    Test

---
+++ app/src/main/java/com/example/gameboardproj/SelectRipActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gameboardproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectRipActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_rip)
+    }
+}
