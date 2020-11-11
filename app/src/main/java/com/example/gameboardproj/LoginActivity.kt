500b74438ec9f6e486c7eb72b36a46092629e817
bayronPalacio <58010308+bayronPalacio@users.noreply.github.com>
2020-11-11 11:03:28 -0800

    Registration and login activity were changed to store the user data in Mongo using the Volley library

--- app/src/main/java/com/example/gameboardproj/LoginActivity.kt
+++ app/src/main/java/com/example/gameboardproj/LoginActivity.kt
@@ -6,24 +6,25 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_login.password
 import kotlinx.android.synthetic.main.activity_login.register
-
+import org.json.JSONObject
+import java.io.BufferedReader
 
 /**
  * @author: Bayron Arturo Palacio
  *
- * This class handles the login function and check that the user exists in the database.
+ * This class handles the login function and check that the user exists in the Mongo database.
  * Also, it will launch the activity_registration when the text "click here to register" is clicked
  *
  */
 
 class LoginActivity : AppCompatActivity() {
-
 
     private var dataBaseGame: GameDatabase? = null
     private var studentDao: StudentDao? = null
@@ -35,59 +36,97 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //When the login button is clicked
+        var fileReader: BufferedReader = application.assets.open("url.txt").bufferedReader()
+        var url = fileReader.readLine()
+
         loginBtn.setOnClickListener {
-                Observable.fromCallable {
-
-                    //An instance of the Database and studentDao are created
-                    dataBaseGame = GameDatabase.getAppDataBase(context = this)
-                    studentDao = dataBaseGame?.studentDao()
-
-                    with(studentDao) {
-                        //A request is made to the database through the studentDao with the getStudentByEmail() method
-                        studentInfo = this?.getStudentByEmail(inputEmail = username.text.toString())!!
-                    }
-                }.subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe()
-            Thread.sleep(300)
-            //Check if the email and password entered by the user match with the user in the database
-            if(studentInfo?.emailDb.toString() == username.text.toString() && studentInfo?.passwordDb.toString() == password.text.toString() )
-            {
-                Toast.makeText(this,"User Name\n" + studentInfo?.firstNameDb.toString() + " " + studentInfo?.lastNameDb.toString(),Toast.LENGTH_LONG).show()
-
-                // Shared Preferences
-                val sharedPreferences : SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
-                val editor : SharedPreferences.Editor =  sharedPreferences.edit()
-
-                // Store the current user email
-                editor.putString("user_email", username.text.toString())
-                editor.apply()
-                editor.commit()
-
-                if(studentInfo?.roleTypeDb.toString().toInt() == 0)
-                {
-                    //if the user is a leader, the leader activity will be launched
-                    val toLeaderScreen = Intent(this,LeaderMain::class.java)
-                    startActivity(toLeaderScreen)
-                }
-                else{
-                    //if the user is a player, the player activity will be launched
-                    val toUserScreen = Intent(this,UserMain::class.java)
-                    startActivity(toUserScreen)
-                }
+            if(username.text.toString() == "" || password.text.toString() == ""){
+                    Toast.makeText(this, "Please enter information", Toast.LENGTH_LONG).show()
             }
-            //if the information entered does not match with any user in the database, a toast will be shown
+            //If the fields are not empty
             else{
-                Toast.makeText(this,"Please enter a valid username and password",Toast.LENGTH_LONG).show()
+                val user = JSONObject()
+                user.put("email", username.text.toString())
+                user.put("password", password.text.toString())
+
+                var urlPath = "$url/login"
+                val que = Volley.newRequestQueue(this)
+                val req = JsonObjectRequest(
+                    Request.Method.POST, urlPath, user,
+                    Response.Listener { response ->
+                        if(response["responseLogin"].toString().equals("Wrong Password")){
+                            Toast.makeText(this, "Your account or password is incorrect.", Toast.LENGTH_LONG).show()
+                        }
+                        else if(response["responseLogin"].toString().equals("Right User")){
+                            Toast.makeText(this, "Welcome " + response["name"] + " " + response["last"], Toast.LENGTH_LONG).show()
+                            val toLogin = Intent(this,LoginActivity::class.java)
+                            startActivity(toLogin)
+                        }
+                        else if(response["responseLogin"].toString().equals("User does not exist")){
+                            Toast.makeText(this, "This account does not exist", Toast.LENGTH_LONG).show()
+                        }
+                        println("Response from server -> " + response["responseLogin"])
+                    }, Response.ErrorListener {
+                        println("Error from server")
+                    }
+                )
+                que.add(req)
             }
         }
-
 
         //The Register Activity is launched, when the text "click here to register" is clicked
         register.setOnClickListener {
             val toRegister = Intent(this, RegistrationActivity::class.java)
             startActivity(toRegister)
         }
+
+        //************************************************ -ROOM DATABASE- *************************************************************//
+        //When the login button is clicked
+//        loginBtn.setOnClickListener {
+//                Observable.fromCallable {
+//
+//                    //An instance of the Database and studentDao are created
+//                    dataBaseGame = GameDatabase.getAppDataBase(context = this)
+//                    studentDao = dataBaseGame?.studentDao()
+//
+//                    with(studentDao) {
+//                        //A request is made to the database through the studentDao with the getStudentByEmail() method
+//                        studentInfo = this?.getStudentByEmail(inputEmail = username.text.toString())!!
+//                    }
+//                }.subscribeOn(Schedulers.io())
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe()
+//            Thread.sleep(300)
+//            //Check if the email and password entered by the user match with the user in the database
+//            if(studentInfo?.emailDb.toString() == username.text.toString() && studentInfo?.passwordDb.toString() == password.text.toString() )
+//            {
+//                Toast.makeText(this,"User Name\n" + studentInfo?.firstNameDb.toString() + " " + studentInfo?.lastNameDb.toString(),Toast.LENGTH_LONG).show()
+//
+//                // Shared Preferences
+//                val sharedPreferences : SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+//                val editor : SharedPreferences.Editor =  sharedPreferences.edit()
+//
+//                // Store the current user email
+//                editor.putString("user_email", username.text.toString())
+//                editor.apply()
+//                editor.commit()
+//
+//                if(studentInfo?.roleTypeDb.toString().toInt() == 0)
+//                {
+//                    //if the user is a leader, the leader activity will be launched
+//                    val toLeaderScreen = Intent(this,LeaderMain::class.java)
+//                    startActivity(toLeaderScreen)
+//                }
+//                else{
+//                    //if the user is a player, the player activity will be launched
+//                    val toUserScreen = Intent(this,UserMain::class.java)
+//                    startActivity(toUserScreen)
+//                }
+//            }
+//            //if the information entered does not match with any user in the database, a toast will be shown
+//            else{
+//                Toast.makeText(this,"Please enter a valid username and password",Toast.LENGTH_LONG).show()
+//            }
+//        }
     }
 }
--- app/src/main/java/com/example/gameboardproj/RegistrationActivity.kt
+++ app/src/main/java/com/example/gameboardproj/RegistrationActivity.kt
@@ -1,29 +1,37 @@
 package com.example.gameboardproj
 
+import android.R.attr.data
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.Observable
-import kotlinx.android.synthetic.main.activity_login.*
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.gameboardproj.data.User
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import kotlinx.android.synthetic.main.activity_registration.*
-import kotlinx.android.synthetic.main.activity_registration.password
-import kotlinx.android.synthetic.main.activity_registration.register
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.util.*
+
 
 /**
  * @author: Bayron Arturo Palacio
  *
- * This class will create a new user with the data entered by the user and add it to the Database.
- *
+ * This class will create a new user with the data entered by the user and add it to the Mongo Database.
  *
  */
 
 class RegistrationActivity : AppCompatActivity() {
-
 
     private var dataBaseGame: GameDatabase? = null
     private var studentDao: StudentDao? = null
@@ -36,56 +44,134 @@ class RegistrationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-        val sharedPreferences : SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        var fileReader: BufferedReader = application.assets.open("url.txt").bufferedReader()
+        var url = fileReader.readLine()
+
+        val sharedPreferences : SharedPreferences = this.getSharedPreferences(
+            sharedPrefFile,
+            Context.MODE_PRIVATE
+        )
         val editor : SharedPreferences.Editor =  sharedPreferences.edit()
 
-        //When the user clicks the Register Button:
         register.setOnClickListener {
-            //The fields are check to make sure that are not empty. If they are empty, a toast will be shown
             if(firstName.text.toString() == "" || lastName.text.toString() == "" || email.text.toString() == "" || password.text.toString() == "" || radioGroupRole.checkedRadioButtonId == -1){
                 if(radioGroupRole.checkedRadioButtonId == -1){
-                    Toast.makeText(this,"Please select a role",Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Please select a role", Toast.LENGTH_LONG).show()
                 }
                 else {
-                    Toast.makeText(this,"Please enter information",Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Please enter information", Toast.LENGTH_LONG).show()
                 }
             }
             //If the fields are not empty
             else{
-                // add info to SharedPreferences - user fName, lName
-                editor.putString("user_firstName", firstName.text.toString())
-                editor.putString("user_lastName", lastName.text.toString())
-                editor.apply()
-                editor.commit()
-
                 role =
                     if(playerRole.isChecked){
                     1 // One will be assigned for the player user
                 } else {
                     0 // Zero will be assigned for the leader user
                 }
-                Observable.fromCallable {
+                var urlPath = "$url/adduser"
 
-                    //An instance of the Database and studenDao are created
-                    dataBaseGame = GameDatabase.getAppDataBase(context = this)
-                    studentDao = dataBaseGame?.studentDao()
+                val newUser = JSONObject()
 
-                    //An Student object is created with the information entered by the user
-                    var student1 = Student(firstNameDb = firstName.text.toString(), lastNameDb = lastName.text.toString(), emailDb = email.text.toString(), passwordDb = password.text.toString(),roleTypeDb = role)
+                newUser.put("firstName", firstName.text.toString())
+                newUser.put("lastName", lastName.text.toString())
+                newUser.put("email", email.text.toString())
+                newUser.put("password", password.text.toString())
+                newUser.put("role", role)
 
-                    with(studentDao) {
-                        //The student object is added to the database through the studentDao with the insertStudent() method
-                        this?.insertStudent(student1)
+                val que = Volley.newRequestQueue(this)
+                val req = JsonObjectRequest(
+                    Request.Method.POST, urlPath, newUser,
+                    Response.Listener { response ->
+                        if(response["responseServer"].toString().equals("Yes")){
+                            Toast.makeText(this, "User has been created", Toast.LENGTH_LONG).show()
+                            val toLogin = Intent(this,LoginActivity::class.java)
+                            startActivity(toLogin)
+                        }
+                        else{
+                            Toast.makeText(this, "User already exits", Toast.LENGTH_LONG).show()
+                        }
+                        println("Response from server -> " + response["responseServer"])
+                    }, Response.ErrorListener {
+                        println("Error from server")
                     }
-                }.subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe()
-                //Message to show the new user has been created
-                Toast.makeText(this,"A new user has been created",Toast.LENGTH_LONG).show()
-                //Launch the Login Activity
-                val toLogin = Intent(this,LoginActivity::class.java)
-                startActivity(toLogin)
+                )
+                que.add(req)
+
+//                Log.i("data",dataJson)
+
+//                val queue = Volley.newRequestQueue(this)
+//////                val url = "https://www.google.com"
+////
+////// Request a string response from the provided URL.
+//                val stringRequest = StringRequest(
+//                    Request.Method.GET, url,
+//                    Response.Listener<String> { response ->
+//                        // Display the first 500 characters of the response string.
+//                        Log.i("Res",response)
+////                        textView.text = "Response is: ${response.substring(0, 500)}"
+//                    },
+//
+//                    Response.ErrorListener { Log.i("res","did not work")})
+//
+//// Add the request to the RequestQueue.
+//                queue.add(stringRequest)
+
+
             }
         }
+
+
+
+        //************************************************ -ROOM DATABASE- *************************************************************//
+        //When the user clicks the Register Button: ROOM DATABASE
+//        register.setOnClickListener {
+////            //The fields are check to make sure that are not empty. If they are empty, a toast will be shown
+////            if(firstName.text.toString() == "" || lastName.text.toString() == "" || email.text.toString() == "" || password.text.toString() == "" || radioGroupRole.checkedRadioButtonId == -1){
+////                if(radioGroupRole.checkedRadioButtonId == -1){
+////                    Toast.makeText(this,"Please select a role",Toast.LENGTH_LONG).show()
+////                }
+////                else {
+////                    Toast.makeText(this,"Please enter information",Toast.LENGTH_LONG).show()
+////                }
+////            }
+////            //If the fields are not empty
+////            else{
+////                // add info to SharedPreferences - user fName, lName
+////                editor.putString("user_firstName", firstName.text.toString())
+////                editor.putString("user_lastName", lastName.text.toString())
+////                editor.apply()
+////                editor.commit()
+////
+////                role =
+////                    if(playerRole.isChecked){
+////                    1 // One will be assigned for the player user
+////                } else {
+////                    0 // Zero will be assigned for the leader user
+////                }
+////                Observable.fromCallable {
+////
+////                    //An instance of the Database and studenDao are created
+////                    dataBaseGame = GameDatabase.getAppDataBase(context = this)
+////                    studentDao = dataBaseGame?.studentDao()
+////
+////                    //An Student object is created with the information entered by the user
+////                    var student1 = Student(firstNameDb = firstName.text.toString(), lastNameDb = lastName.text.toString(), emailDb = email.text.toString(), passwordDb = password.text.toString(),roleTypeDb = role)
+////
+////                    with(studentDao) {
+////                        //The student object is added to the database through the studentDao with the insertStudent() method
+////                        this?.insertStudent(student1)
+////                    }
+////                }.subscribeOn(Schedulers.io())
+////                    .observeOn(AndroidSchedulers.mainThread())
+////                    .subscribe()
+////                //Message to show the new user has been created
+////                Toast.makeText(this,"A new user has been created",Toast.LENGTH_LONG).show()
+////                //Launch the Login Activity
+////                val toLogin = Intent(this,LoginActivity::class.java)
+////                startActivity(toLogin)
+////            }
+//        }
     }
 }
